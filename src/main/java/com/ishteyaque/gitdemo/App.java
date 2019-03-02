98a359087fe8e37926f7050b053f64f51fa50eef
ishteyaque <ahmad,shteyaque966@gmail.com>
2019-03-02 20:52:22 +0530

    project initial setup

---
+++ src/main/java/com/ishteyaque/gitdemo/App.java
@@ -0,0 +1,13 @@
+package com.ishteyaque.gitdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
