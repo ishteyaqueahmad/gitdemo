ba4d5b68165777a88f0f6becabbb810641a730da
ishteyaque <ahmad,shteyaque966@gmail.com>
2019-03-03 11:36:19 +0530

    App updated

--- src/main/java/com/ishteyaque/gitdemo/App.java
+++ src/main/java/com/ishteyaque/gitdemo/App.java
@@ -12,5 +12,6 @@ public class App
         System.err.println("This is error");
         System.out.println("Working fine");
         System.out.println("All done");
+        System.out.println("Quite fine");
     }
 }
