6939a5cd8e85e90eabfa7ce2e3586a581ba052ab
Ishteyaque <ahmad.ishteyaque966@gmail.com>
2022-07-23 20:34:08 +0530

    branch demo

---
+++ src/main/java/com/ishteyaque/gitdemo/GitBranchBasicDemo.java
@@ -0,0 +1,7 @@
+package com.ishteyaque.gitdemo;
+
+public class GitBranchBasicDemo {
+    public static void main(String[] args) {
+        System.out.println("Create feature branch for update");
+    }
+}
